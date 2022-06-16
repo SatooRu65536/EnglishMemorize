13457d09b0c0c182854b52c0f2d568ed629a4bc3
SatooRu <87361015+SatooRu65536@users.noreply.github.com>
2022-06-16 12:54:28 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,8 +1,12 @@
-with open('wordslist2.txt', 'r') as f:
+with open('wordlist.txt', 'r') as f:
     data = f.read().split("\n")
 
 output = '{'
 for i in range(0, len(data), 2):
+    if data[i]:
+        tmp = data[i+1]
+        data[i+1] = data[i+1]
+        data[i] = tmp
     output += f'"{data[i]}": "{data[i+1]}",\n'
 
 output += '}'
